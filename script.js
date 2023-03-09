405b2e0dc3b1bb9a202fc51d875329fd4cdb095b
Jason <jasonkodama@gmail.com>
2023-03-09 10:25:28 -0700

    added for loop

--- script.js
+++ script.js
@@ -12,40 +12,40 @@ function writePassword() {
 }
 
 function generatePassword() {
-//  var username = prompt("Enter Username")
+  //  var username = prompt("Enter Username")
   var passwordLength = parseInt(prompt("Enter password length between 8 to 126"))
-  if(passwordLength < 8 || passwordLength > 126 || isNaN(passwordLength)){
+  if (passwordLength < 8 || passwordLength > 126 || isNaN(passwordLength)) {
     return "Invalid Password length entered..." // || or operator
   }
   var acceptUppercase = confirm("Do you like to have Uppercase alphabets in your password")
   var acceptLowercase = confirm("Do you like to have Lowercased alpabets in your password")
   var acceptSpecialcharacters = confirm("Do you like to have Special Characters in your password")
   var acceptNumbers = confirm("Do you like to have Numbers in your password")
-  if(!acceptLowercase && !acceptSpecialcharacters && !acceptNumbers && !acceptUppercase){
+  if (!acceptLowercase && !acceptSpecialcharacters && !acceptNumbers && !acceptUppercase) {
     return "Please select an option..."
   }
   // ! not operator  && and operator
   var validCharactersInventory = []
 
-  if(acceptLowercase){
+  if (acceptLowercase) {
     validCharactersInventory += "abcdefghijklmnopqrstuvwxyz" // valiCharactersInventory = validCharactersInvenotry + "abs"
   }
-  if(acceptUppercase){
+  if (acceptUppercase) {
     validCharactersInventory += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   }
-  if(acceptSpecialcharacters){
+  if (acceptSpecialcharacters) {
     validCharactersInventory += "!@#$%^&*()+_"
   }
-  if(acceptNumbers){
+  if (acceptNumbers) {
     validCharactersInventory += "1234567890"
   }
   var password = ""
 
-  var index = Math.floor(Math.random() * validCharactersInventory.length)  // 0 to 1 -- o.5 o.75 .3
-  password += validCharactersInventory[index]
+  for (let index = 0; index < passwordLength; index++) {
+    var i = Math.floor(Math.random() * validCharactersInventory.length)  // 0 to 1 -- o.5 o.75 .3
+    password = validCharactersInventory[i]
 
-
-
+  }
   return password;
 
 }
